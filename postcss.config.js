1600bf82d96e49e7537074d22802675c41610071
saranshisatgit <saranshisatgit>
2023-09-12 19:43:07 +0200

    fixed the css

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {
-      theme: {
-        extend: {
-          keyframes: {
-            spinSlow: {
-              '0%': { transform: 'rotate(0deg)' },
-              '100%': { transform: 'rotate(360deg)' },
-            },
-            spinReverse: {
-              '0%': { transform: 'rotate(360deg)' },
-              '100%': { transform: 'rotate(0deg)' },
-            }
-          },
-          animation: {
-            'spin-slow': 'spinSlow 10s linear infinite',
-            'spin-reverse': 'spinReverse 5s linear infinite',
-          }
-        }
-      },
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
