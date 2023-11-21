a9ed5fa19e6f73f951f4d9459763a3f1ab5ece93
saranshisatgit <saranshisatgit>
2023-11-21 20:40:12 +0100

    Inspiration Page Fix css

--- app/inspirations/page.tsx
+++ app/inspirations/page.tsx
@@ -1,19 +1,19 @@
 "use client"
-// pages/inspirations.js
 import React, { Suspense } from 'react';
 import Image from 'next/image';
 import useSWR from 'swr';
 import Loading from './loading';
 import Subscription from '@/components/subscription';
 
-const fetcher = (url:string, options?: RequestInit) => fetch(url, options).then(res => res.json());
+const fetcher = (url: string, options?: RequestInit) => fetch(url, options).then(res => res.json());
 
 const InspirationsPage = () => {
   return (
-    <div className="mx-auto flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl font-bold mb-4">Inspirations: Live </h1>
-      <h2 className="text-1xl font-bold mb-4">These are from our data store live inspirations that we are going to inspire from </h2>
-      <Suspense fallback={<Loading/>}>
+    // Adjusted padding and margins for smaller screens
+    <div className="mx-auto flex min-h-screen flex-col items-center justify-between p-4 sm:p-8 md:p-24">
+      <h1 className="text-xl sm:text-2xl font-bold mb-4">Inspirations: Live </h1>
+      <h2 className="text-lg sm:text-1xl font-bold mb-4">These are from our data store live inspirations that we are going to inspire from </h2>
+      <Suspense fallback={<Loading />}>
         <InspirationsGrid />
       </Suspense>
     </div>
@@ -27,30 +27,30 @@ const InspirationsGrid = () => {
 
   return (
     <>
-    <div className="w-full">
-    {data.data.map((inspiration: any) => (
-      <div key={inspiration.id} className="border rounded-lg p-4 mb-6">
-        <h2 className="text-xl font-semibold mb-4">{inspiration.inspiration_name}</h2>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-          {inspiration.images_for_inspiration.map((image: any) => (
-            <div key={image.id} className="relative w-full h-64">
-              <Suspense fallback={<Loading/>}>
-                <Image
-                src={`https://directus-bucket-jy.s3.us-east-1.amazonaws.com/${image.directus_files_id.filename_disk}`}
-                alt={inspiration.inspiration_name}
-                className="rounded fill cover"
-                fill
-                objectFit='cover'
-              /></Suspense>
+      <div className="w-full">
+        {data.data.map((inspiration: any) => (
+          <div key={inspiration.id} className="border rounded-lg p-4 mb-6">
+            <h2 className="text-lg sm:text-xl font-semibold mb-4">{inspiration.inspiration_name}</h2>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+              {inspiration.images_for_inspiration.map((image: any) => (
+                <div key={image.id} className="relative w-full h-64">
+                  <Suspense fallback={<Loading />}>
+                    <Image
+                      src={`https://directus-bucket-jy.s3.us-east-1.amazonaws.com/${image.directus_files_id.filename_disk}`}
+                      alt={inspiration.inspiration_name}
+                      className="rounded fill cover"
+                      fill
+                      objectFit='cover'
+                    /></Suspense>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-  <Subscription/>
-  </>
-);
+      <Subscription />
+    </>
+  );
 };
 
 export default InspirationsPage;
